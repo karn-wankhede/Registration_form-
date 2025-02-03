116774e8131ca3d330bdb74d32dfceb695418d47
Karan Wankhede <wankhedekaran2002@gmail.com>
2025-02-03 15:10:42 +0530

    Add files via upload

---
+++ Form4.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace project1
+{
+    public partial class Form4 : Form
+    {
+        public Form4()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            textBox1.TextAlign=HorizontalAlignment.Left;
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+            textBox1.TextAlign = HorizontalAlignment.Right;
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+            textBox1.TextAlign = HorizontalAlignment.Center;
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+            textBox1.ForeColor = Color.Red;
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+            textBox1.ForeColor = Color.Blue;
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+            textBox1.ForeColor = Color.Purple;
+        }
+    }
+}
